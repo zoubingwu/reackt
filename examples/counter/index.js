fd6cb556cc08b71b3d645aa4e1bec44d6cdeaa9e
shadeofgod <zoubingwu@gmail.com>
2019-11-24 02:20:15 +0800

    update counter example

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,52 +1,98 @@
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createLogger } from 'redux-logger';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import createStore from '../../';
 
-async function delay(ms = Math.random() * 3000) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+const delay = (ms = Math.random() * 2000) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
-class CounterModel extends Model {
-  name = 'counter'
+const initialState = {
+  count: 0,
+  loading: false,
+};
 
-  state = {
-    count: 0
-  }
+const ns = 'counter';
 
-  computed = {
-    double: this.state.count * 2,
-  }
+const counter = {
+  ns,
+  state: initialState,
+  updates: (setState, getState, dispatch) => {
+    const increment = () =>
+      setState(state => {
+        state.count = state.count + 1;
+      }, 'increment count');
 
-  increment() {
-    this.setState(state => state.count++);
-  }
+    const decrement = () =>
+      setState(state => {
+        state.count = state.count - 1;
+      }, 'decrement count');
 
-  async incrementAsync() {
-    await delay()
-    this.increment()
-  }
-}
+    const incrementN = n =>
+      setState(state => {
+        state.count = state.count + n;
+      }, 'increment count by n');
 
-const store = new Store({
-  models: [CounterModel],
-  middlewares: [],
-  onError: (e) => {
-    if (e.message) {
-      alert(e.message);
-    }
+    const loading = bool =>
+      setState(state => {
+        state.loading = bool;
+      }, `set loading to ${bool}`);
+    const reset = () => {
+      setState(_ => initialState, 'reset state');
+    };
+
+    const incrementAsync = async () => {
+      loading(true);
+      await delay();
+      if (Math.random() < 0.5) {
+        throw new Error('oops');
+      }
+      increment();
+      loading(false);
+    };
+
+    return {
+      increment,
+      incrementN,
+      decrement,
+      incrementAsync,
+      reset,
+    };
   },
-})
+};
 
-function Counter() {
-  const counterModel = useModel('counter', model => [
-    model.state.count,
-  ]);
+const logger = createLogger({
+  collapsed: true,
+});
+
+const store = createStore({
+  models: [counter],
+  onError: e => alert(e.message),
+  middlewares: [logger],
+});
+
+function App() {
+  const { count, loading } = useSelector(state => state.counter);
+  const {
+    counter: { increment, incrementAsync, decrement, reset },
+  } = useDispatch();
 
   return (
     <div>
-      <h1>count: {counterModel.state.count}</h1>
-      <h1>double: {counterModel.computed.double}</h1>
-      <button onClick={counterModel.increment}>increment</button>
-      <button onClick={counterModel.incrementAsync}>increment async</button>
+      <h1> {count} </h1> <button onClick={increment}> increment </button> <hr />
+      <button onClick={incrementAsync}> increment async </button>
+      {loading && 'loading...'} <hr />
+      <button onClick={decrement}> decrement </button> <hr />
+      <button onClick={reset}> reset </button>
     </div>
-  )
+  );
 }
 
+const rootElement = document.getElementById('app');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
